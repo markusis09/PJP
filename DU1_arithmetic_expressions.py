af12550902d6156ea443f824828d8dc68236df96
markusis09 <marek.tengler@seznam.cz>
2024-02-26 23:20:57 +0100

    own eval function

--- DU1_arithmetic_expressions.py
+++ DU1_arithmetic_expressions.py
@@ -1,6 +1,32 @@
 
+def operation(a, b, operator):
+    if operator == '*':
+        return a * b
+    elif operator == '/':
+        return a / b
+    elif operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+
+def build_new_expression(expression, i):
+    return expression[:i-1] + str(operation(int(expression[i-1]), int(expression[i+1]), expression[i])) + expression[i+2:]
+
 def compute_expression(expression): # 1+2*3+4
-    return eval(expression)
+    i = 0
+    while i < len(expression):
+        if(expression[i] == '*' or expression[i] == '/'):
+            expression = compute_expression(build_new_expression(expression, i))
+            break
+        i+=1
+    i = 0
+    while i < len(expression):
+        if(expression[i] == '+' or expression[i] == '-'):
+            expression = compute_expression(build_new_expression(expression, i))
+            break
+        i+=1
+    return expression
+
 
 def remove_brackets(expression): # 1*(3+5) / 2 + 1 * 4 + ((4+5) * 2) +1
     i = 0
@@ -17,7 +43,8 @@ def remove_brackets(expression): # 1*(3+5) / 2 + 1 * 4 + ((4+5) * 2) +1
 
 rows_count = 1
 for i in range(rows_count):
-    expression = '1+(2*(3+4)+5)'
+    expression = '(2*(3+4)+5)'
     expression = expression.replace(' ', '')
     expression = remove_brackets(expression)
+    expression = compute_expression(expression)
     print(expression)
